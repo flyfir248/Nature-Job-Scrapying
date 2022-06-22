8381e6a5fed26f4c77e2183e1e21325aafa99974
Anoop Johny <57437701+flyfir248@users.noreply.github.com>
2022-06-22 17:39:32 +0200

    Nature scrapy project
    I successfully scraped the Nature job portal using scraping along the search lines of Bioinformatics, Lifescience, and global search.

---
+++ takenature2.py
@@ -0,0 +1,31 @@
+import scrapy
+
+
+# search involving life science in germany
+class takenature2(scrapy.Spider):
+    
+    name="naturetaker"
+    start_urls=['https://www.nature.com/naturecareers/jobs/search?text=Life+Science&location=Germany']
+    
+    
+    def parse(self,response):
+        for article in response.css('article'):
+            try:
+                yield{
+                    'Title' :article.css('h3.p-card__info-title ::text').get(),
+                    'Subtitle' :article.css('p.p-card__info-subtitle ::text').get(),
+                    'Location':article.css('p.p-card__info-location ::text').get(),
+                    'Time' : article.css('p.p-card__info-time::text').get()
+                }
+            except:
+                yield{
+                    'Title' :article.css('h3.p-card__info-title ::text').get(),
+                    'Subtitle' :article.css('p.p-card__info-subtitle ::text').get(),
+                    'Location':article.css('p.p-card__info-location ::text').get(),
+                    'Time': 'Time not mentioned'
+                }
+            
+            # parse to next page
+            next_page=response.css('a.c-pagination__item.c-pagination__item--arrow-right').attrib['href']
+            if next_page is not None:
+                yield response.follow(next_page,callback=self.parse)
